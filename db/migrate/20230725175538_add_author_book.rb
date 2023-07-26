61c5ad090915a329317b81128419b357a67ede86
Camilo Rocha <camilorocha11@gmail.com>
2023-07-26 11:06:01 -0300

    fix migrate

--- db/migrate/20230725175538_add_author_book.rb
+++ db/migrate/20230725175538_add_author_book.rb
@@ -1,6 +1,6 @@
 class AddAuthorBook < ActiveRecord::Migration[7.0]
   def change
-    create_table :author_books do |t|
+    create_table :authors_books do |t|
       t.references :author, null: false, foreign_key: true
       t.references :book, null: false, foreign_key: true
       t.timestamps
